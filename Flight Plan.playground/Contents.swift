fc1f23789ce40a7a9d101b1e60b48eb0dbfc833e
kneskoromny <kneskoromny@gmail.com>
2021-10-01 11:53:22 +0300

    work with nested structs, enums, dates

---
+++ Flight Plan.playground/Contents.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+let json = """
+{
+    "aircraft": {
+        "identification": "NA12345",
+        "color": "Blue/White"
+    },
+    "route": ["KTTD", "KHIO"],
+    "departure_time": {
+        "proposed": "2018-04-20T15:07:24-07:00",
+        "actual": "2018-04-20T15:07:24-07:00"
+    },
+    "flight_rules": "IFR",
+    "remarks": null
+}
+""".data(using: .utf8)!
+
+var decoder = JSONDecoder()
+decoder.dateDecodingStrategy = .iso8601
+do {
+    let plan = try decoder.decode(FlightPlan.self, from: json)
+    print(plan.aircraft.identification)
+    print(plan.actualDepartureDate!)
+} catch {
+    print("JSON decoding error: \(error)")
+}
